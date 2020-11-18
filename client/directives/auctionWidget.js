9510a212bcbb63d01aa1f3dab5dbbee1b9345eda
koshaapatel <koshapatel1045@gmail.com>
2020-11-18 18:12:36 -0500

    Active auction details are fetched from database and is rendered.

--- client/directives/auctionWidget.js
+++ client/directives/auctionWidget.js
@@ -12,34 +12,34 @@ auctionApp.directive("auctionWidget", ['LoggedInCheck','ipCookie','host','$http'
             '</div>'+
             '</div>'+
             '</div>'+
-            '<div class="row" ng-if="activeAuction">'+
+            '<div class="row" ng-repeat="auction in auctionArr">'+
             '<div class="col-md-12 statsDiv">'+
-            '<label>Seller :</label> {{ activeAuction.userName }}'+
+            '<label>Seller :</label> {{ auction.userName }}'+
             '</div>'+
             '<div class="col-md-12 auctionItemDiv alignCenter">'+
             '<div class="col-md-6">'+
             '<i class="fa fa-qrcode" style="font-size: 35px;"></i><br/>'+
-            '{{ activeAuction.itemName }}'+
+            '{{ auction.itemName }}'+
             '</div>'+
             '<div class="col-md-6">'+
             '<label>Quantity</label><br/>'+
-            '{{ activeAuction.itemQuantity }}'+
+            '{{ auction.itemQuantity }}'+
             '</div>'+
             '</div>'+
             '<div class="col-md-12 auctionItemDiv">'+
-            '<label>Time Left :</label> {{ timeRemaining | number:0 || "0" }}'+
+            '<label>Time Left :</label> {{ timeRemainin | number:0 || "0" }}'+
             '</div>'+
             '<div class="col-md-12 auctionItemDiv">'+
-            '<label>Minimum Bid :</label> {{ activeAuction.minBid }}'+
+            '<label>Minimum Bid :</label> {{ auction.minBid }}'+
             '</div>'+
-            '<div class="col-md-12 auctionItemDiv alignCenter" ng-show="activeAuction.userName != presentUser.userName">'+
+            '<div class="col-md-12 auctionItemDiv alignCenter" ng-show="auction.userName != presentUser.userName">'+
             '<input autocomplete="off" class="paper-input paper-input--floating" type="number" id="bidVal"/>'+
             '<label>Enter value for Bid</label>'+
             '</div>'+
-            '</div>'+
             '<div class="row" ng-if="activeAuction">'+
-            '<div class="col-md-12 auctionItemDiv alignCenter" ng-show="activeAuction.userName != presentUser.userName">'+
+            '<div class="col-md-12 auctionItemDiv alignCenter" ng-show="auction.userName != presentUser.userName">'+
             '<button class="btn btn-primary auctionButton" ng-click="placeBid()"><i class="fa fa-money"></i>Place Bid</button>'+
+            '</div>'+
             '</div>'+
             '</div>'+
             '<div class="row" ng-if="!activeAuction">'+
@@ -48,7 +48,18 @@ auctionApp.directive("auctionWidget", ['LoggedInCheck','ipCookie','host','$http'
             '</div>'+
             '</div>',
         link : function (scope, $element, attrs) {
-
+            (function showAuction() {
+                $http({
+                    url: host + "auctions/find/" ,
+                    method: "GET",
+                    crossDomain: true
+                }).success(function (data, status, headers, config) {
+                    scope.auctionArr = data;
+                }).error(function (data, status, headers, config) {
+                    console.log("Error In get auction for user", data);
+                    scope.message = data.message;
+                });
+            }());
         }
     };
 }]);
